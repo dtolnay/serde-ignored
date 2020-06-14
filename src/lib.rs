7973822a0759d3383145c5f32ac5f71442c07f4d
David Tolnay <dtolnay@gmail.com>
2020-06-13 22:55:58 -0700

    Fix hyphen to underscore in crate name

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! [![github]](https://github.com/dtolnay/serde-ignored)&ensp;[![crates-io]](https://crates.io/crates/serde-ignored)&ensp;[![docs-rs]](https://docs.rs/serde-ignored)
+//! [![github]](https://github.com/dtolnay/serde-ignored)&ensp;[![crates-io]](https://crates.io/crates/serde_ignored)&ensp;[![docs-rs]](https://docs.rs/serde_ignored)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
